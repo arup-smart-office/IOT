1bb9412f55ff6e9dafc255f7cb8d384acc68a240
rnphilp <rnphilp@users.noreply.github.com>
2019-05-10 12:44:59 +0100

    add dynamic data to api request

--- api/api.py
+++ api/api.py
@@ -1,24 +1,29 @@
 import requests
 from aws_exports import awsmobile
 
+ID = 1
+previous_version = 16
+is_occupied = False
+
 headers = {"x-api-key": awsmobile["aws_appsync_apiKey"]}
 
-def postData(query):
-    request = requests.post(awsmobile["aws_appsync_graphqlEndpoint"], json={'query': query}, headers=headers)
+def postData(query, variables):
+    request = requests.post(awsmobile["aws_appsync_graphqlEndpoint"], json={'query': query, 'variables': variables}, headers=headers)
     print(request, '<<< request')
     print(request.status_code, '<<<< status code')
     if request.status_code == 200:
       return request.json()
 
 query = """
-  mutation {
-    updateDesk(input: {id:1, expectedVersion:14 isOccupied:true}) {
+  mutation UpdateDesk($input: UpdateDeskInput!) {
+    updateDesk(input: $input) {
       id
       isOccupied
       version
 	  }
   }
 """
+variables = {'input': {'id': ID, 'expectedVersion': previous_version, 'isOccupied': is_occupied}}
 
-result = postData(query)
+result = postData(query, variables)
 print(result, '<<<< result')
