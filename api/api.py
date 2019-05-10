ac33c10d6eb08dcb4a917641de2a206425466c1e
rnphilp <rnphilp@users.noreply.github.com>
2019-05-10 15:15:10 +0100

    adds getDesk api function

--- api/api.py
+++ api/api.py
@@ -1,20 +1,10 @@
 import requests
 from aws_exports import awsmobile
 
-ID = 1
-previous_version = 16
-is_occupied = False
-
 headers = {"x-api-key": awsmobile["aws_appsync_apiKey"]}
 
-def postData(query, variables):
-    request = requests.post(awsmobile["aws_appsync_graphqlEndpoint"], json={'query': query, 'variables': variables}, headers=headers)
-    print(request, '<<< request')
-    print(request.status_code, '<<<< status code')
-    if request.status_code == 200:
-      return request.json()
-
-query = """
+def postDesk(variables):
+  query = """
   mutation UpdateDesk($input: UpdateDeskInput!) {
     updateDesk(input: $input) {
       id
@@ -22,8 +12,24 @@ query = """
       version
 	  }
   }
-"""
-variables = {'input': {'id': ID, 'expectedVersion': previous_version, 'isOccupied': is_occupied}}
+  """
+  request = requests.post(awsmobile["aws_appsync_graphqlEndpoint"], json={'query': query, 'variables': variables}, headers=headers)
+  print(request, '<<< request')
+  print(request.status_code, '<<<< status code')
+  if request.status_code == 200:
+    return request.json()
 
-result = postData(query, variables)
-print(result, '<<<< result')
+
+def getDesk(id):
+  print(id)
+  query = """
+  query GetDesk($id: ID!) {
+    getDesk(id: $id) {
+      version
+	  }
+  }
+  """
+  request = requests.post(awsmobile["aws_appsync_graphqlEndpoint"], json={'query': query, 'variables': {'id': id}}, headers=headers)
+  if request.status_code == 200:
+    return request.json()
+
